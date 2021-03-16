0744c69467614d8490b11c8041d0c708352c7feb
mark.tiahrt@concentrix.com <mark.tiahrt@outlook.com>
2021-03-16 15:18:40 -0600

    added watchpg and Simplifier

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const pg = require("pg");
 const express = require("express");
 const { postgraphile, makePluginHook } = require("postgraphile");
 const { default: PgPubsub } = require("@graphile/pg-pubsub");
+const PgSimplifyInflectorPlugin = require("@graphile-contrib/pg-simplify-inflector");
+
 const app = express();
 require("dotenv").config();
 const pluginHook = makePluginHook([PgPubsub]);
-
-// const pgPool = new pg.Pool({
-//   connectionString: $RDS_URL,
-// });
 
 const pgPool = new pg.Pool({
   connectionString: process.env.RDS_URL,
@@ -17,8 +15,10 @@ const pgPool = new pg.Pool({
 try {
   app.use(
     postgraphile(pgPool, ["public"], {
+      appendPlugins: [PgSimplifyInflectorPlugin],
       pluginHook,
       subscriptions: true,
+      watchPg: true,
       simpleSubscriptions: true,
       graphiql: true,
       enhanceGraphiql: true,
