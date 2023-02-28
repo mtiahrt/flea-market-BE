cd918f172f2f1ff7b802891da64d3945a85a4079
Mark Tiahrt <mark.tiahrt@outlook.com>
2023-02-28 15:48:25 -0700

    removed jwt if in dev mode.  added .env.production

--- server.js
+++ server.js
@@ -23,17 +23,20 @@ try {
         }));
     }
 
-    app.use((req, res, next) => {
-        if(req.originalUrl === "/user/generateAccessToken"){
-            next();
-            return
-        }
-        const tokenValid = isAccessTokenValid(req, res);
-        if(tokenValid !== true){
-            return res.status(401).send(tokenValid);
-        }
-        next()
-    })
+    if(!process.env.DEVELOPMENT){
+        app.use((req, res, next) => {
+            if(req.originalUrl === "/user/generateAccessToken"){
+                next();
+                return
+            }
+            const tokenValid = isAccessTokenValid(req, res);
+            if(tokenValid !== true){
+                return res.status(401).send(tokenValid);
+            }
+            next()
+        })
+    }
+
 
     const admin = require("firebase-admin");
     admin.initializeApp({
