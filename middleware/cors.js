8537a595dd556684d175cb181aaa3f7504254e34
Mark Tiahrt <mark.tiahrt@outlook.com>
2023-05-12 12:12:36 -0600

    more cors

--- middleware/cors.js
+++ middleware/cors.js
@@ -2,7 +2,8 @@ const cors = require('cors');
 const allowedOrigin = process.env.DEVELOPMENT ==='true' ? 'https://localhost:3000' : 'https://shopwildheather.com';
 
 const myCors = cors({
-    origin: allowedOrigin
+    origin: '*'
+    // origin: allowedOrigin
 })
 
 module.exports = myCors;
