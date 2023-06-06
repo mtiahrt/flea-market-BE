94399283043e3b9cbe70ff0fa8c03d184e141ac3
Mark Tiahrt <mark.tiahrt@outlook.com>
2023-06-06 09:55:08 -0600

    added www

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,5 +1,5 @@
 const cors = require('cors');
-const allowedOrigin = process.env.DEVELOPMENT ==='true' ? 'https://localhost:3000' : 'https://marktiahrt.com';
+const allowedOrigin = process.env.DEVELOPMENT ==='true' ? 'https://localhost:3000' : 'https://www.marktiahrt.com';
 
 const myCors = cors({
     // origin: '*'
