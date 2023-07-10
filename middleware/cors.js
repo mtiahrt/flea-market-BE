5afe4709573f141df2cc815a0609bc783827be4e
Mark Tiahrt <mark.tiahrt@outlook.com>
2023-07-10 14:36:39 -0600

    trouble shooting cors error

--- middleware/cors.js
+++ middleware/cors.js
@@ -2,8 +2,8 @@ const cors = require('cors');
 const allowedOrigin = process.env.DEVELOPMENT ==='true' ? 'https://localhost:3000' : 'https://www.marktiahrt.com';
 
 const myCors = cors({
-    // origin: '*'
-    origin: allowedOrigin
+    origin: '*'
+    // origin: allowedOrigin
 })
 
 module.exports = myCors;
