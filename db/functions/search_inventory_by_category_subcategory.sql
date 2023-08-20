d061e62640e41a36982d4c073852539e96b02094
Mark Tiahrt <mark.tiahrt@outlook.com>
2023-08-20 17:46:33 -0600

    fixed bug with search functions

--- db/functions/search_inventory_by_category_subcategory.sql
+++ db/functions/search_inventory_by_category_subcategory.sql
@@ -1,10 +1,10 @@
-create or replace function search_inventory_by_category_subcategory(category_id int4, subcategory_id int4) returns setof fleamarket.inventory_by_category_with_cart as $$
+create or replace function search_inventory_by_category_subcategory(categoryId int4, subcategoryId int4) returns setof fleamarket.inventory_by_category_with_cart as $$
     	select * from fleamarket.inventory_by_category_with_cart
-    	where category_id = category_id
-		and subcategory_id = subcategory_id;
+    	where category_id = categoryId
+		and subcategory_id = subcategoryId;
  $$ language sql stable;
 
-create or replace function search_inventory_by_category(category_id int4) returns setof fleamarket.inventory_by_category_with_cart as $$
+create or replace function search_inventory_by_category(categoryId int4) returns setof fleamarket.inventory_by_category_with_cart as $$
     	select * from fleamarket.inventory_by_category_with_cart
-    	where category_id = category_id
+    	where category_id = categoryId
  $$ language sql stable;
