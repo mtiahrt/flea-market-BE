509cae8e24e2ae6939474fdaceefbe657e3cbdae
Mark Tiahrt <mark.tiahrt@outlook.com>
2023-08-22 14:35:12 -0600

    added fleamarket schema to script

--- db/functions/search_inventory_by_category_subcategory.sql
+++ db/functions/search_inventory_by_category_subcategory.sql
@@ -1,10 +1,10 @@
-create or replace function search_inventory_by_category_subcategory(categoryId int4, subcategoryId int4) returns setof fleamarket.inventory_by_category_with_cart as $$
+create or replace function fleamarket.search_inventory_by_category_subcategory(categoryId int4, subcategoryId int4) returns setof fleamarket.inventory_by_category_with_cart as $$
     	select * from fleamarket.inventory_by_category_with_cart
     	where category_id = categoryId
 		and subcategory_id = subcategoryId;
  $$ language sql stable;
 
-create or replace function search_inventory_by_category(categoryId int4) returns setof fleamarket.inventory_by_category_with_cart as $$
+create or replace function fleamarket.search_inventory_by_category(categoryId int4) returns setof fleamarket.inventory_by_category_with_cart as $$
     	select * from fleamarket.inventory_by_category_with_cart
     	where category_id = categoryId
  $$ language sql stable;
